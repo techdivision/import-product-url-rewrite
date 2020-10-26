cc5311d29fb3dfb22a215e03d2c55f345e171085
Marcus Döllerer <m.doellerer@techdivision.com>
2020-10-26 11:44:27 +0100

    PAC-252 - Move constant CATALOG_SEO_SAVE_REWRITES_HISTORY to import-product-url-rewrite

---
+++ src/Utils/CoreConfigDataKeys.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * TechDivision\Import\Product\UrlRewrite\Utils\CoreConfigDataKeys
+ *
+ * @author    Marcus Döllerer <m.doellerer@techdivision.com>
+ * @copyright 2020 TechDivision GmbH <info@techdivision.com>
+ * @link      https://www.techdivision.com
+ */
+
+namespace TechDivision\Import\Product\UrlRewrite\Utils;
+
+/**
+ * Utility class containing the keys Magento uses to persist values in the "core_config_data table".
+ *
+ * @author    Marcus Döllerer <m.doellerer@techdivision.com>
+ * @copyright 2020 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/techdivision/import-product-url-rewrite
+ * @link      https://www.techdivision.com
+ */
+class CoreConfigDataKeys extends \TechDivision\Import\Product\Utils\CoreConfigDataKeys
+{
+
+    /**
+     * Name for the column 'catalog/seo/generate_category_product_rewrites'.
+     *
+     * @var string
+     */
+    const CATALOG_SEO_GENERATE_CATEGORY_PRODUCT_REWRITES = 'catalog/seo/generate_category_product_rewrites';
+}
